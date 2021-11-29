189d7400de63ef035fa4db3b1cfbb4eae11cb494
Stygg <48440436+VIA-Jesper@users.noreply.github.com>
2021-11-29 20:28:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>- IT-SEP-6-A21 -</p>
-        <p>  action test </p>
+        <p> {process.env.REACT_APP_NOT_SECRET_CODE} </p>
         <a
           className="App-link"
           href="https://reactjs.org"
