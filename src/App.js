5c1e77f52e6ea3a7703a33e718d6072b7eae9f4f
Stygg <48440436+VIA-Jesper@users.noreply.github.com>
2021-11-30 09:25:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>- IT-SEP-6-A21-</p>
         <p> {process.env.REACT_APP_NOT_SECRET_CODE} </p>
         <pre> {JSON.stringify(process.env, undefined, 2)} </pre> 
         <a
